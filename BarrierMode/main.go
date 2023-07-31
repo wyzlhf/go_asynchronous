4689b321a45ab7bb108672343bb55c58089722b8
wyzlhf <haifeng0130@126.com>
2023-07-31 08:42:30 +0800

    init

--- BarrierMode/main.go
+++ BarrierMode/main.go
@@ -1,3 +1,33 @@
 package main
 
+import (
+	"io/ioutil"
+	"net/http"
+	"time"
+)
 
+type BarrierResponse struct {
+	Err    error
+	Resp   string
+	Status int
+}
+
+//构造请求
+
+func doRequest(out chan<- BarrierResponse, url string) {
+	res := BarrierResponse{}
+	client := http.Client{
+		Timeout: time.Duration(20 * time.Second),
+	}
+	resp, err := client.Get(url)
+	if resp != nil {
+		res.Status = resp.StatusCode
+	}
+	if err != nil {
+		res.Err = err
+		out <- res
+		return
+	}
+
+	byt, err := ioutil.ReadAll(resp.Body)
+}
